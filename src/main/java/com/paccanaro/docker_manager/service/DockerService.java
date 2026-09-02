f10eb263d2466ba1e889ac556e7d707455748d08
pacsss <paccanaro18@gmail.com>
2026-09-02 14:01:12 -0300

    feat: implementar DockerService com operações de containers

---
+++ src/main/java/com/paccanaro/docker_manager/service/DockerService.java
@@ -0,0 +1,46 @@
+package com.paccanaro.docker_manager.service;
+
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.api.model.Image;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DockerService {
+
+    private final DockerClient dockerClient;
+
+    public DockerService(DockerClient client) {
+        this.dockerClient = client;
+    }
+
+    public List<Container> listContainers(Boolean all) {
+        return dockerClient.listContainersCmd().withShowAll(all).exec();
+    }
+
+    public List<Image> listImages() {
+        return dockerClient.listImagesCmd().exec();
+    }
+
+    public void startContainer(String containerId) {
+        dockerClient.startContainerCmd(containerId).exec();
+    }
+
+    public void stopContainer(String containerId) {
+        dockerClient.stopContainerCmd(containerId).exec();
+    }
+
+    public void DeleteContainer(String containerId) {
+        dockerClient.removeContainerCmd(containerId).exec();
+    }
+
+    public void createContainer(String imageName) {
+        dockerClient.createContainerCmd(imageName).exec();
+    }
+
+
+}
